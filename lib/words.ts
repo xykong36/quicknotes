022d57075569c4c3f51e3f6dfc74d94f11cc3ee5
XIANGYU KONG <xkongtech@outlook.com>
2024-11-25 22:02:13 -0500

    no public env variables

--- lib/words.ts
+++ lib/words.ts
@@ -3,8 +3,8 @@ import { Word, Difficulty, Example } from "./types";
 import { Database } from "@/app/types/Supabase";
 
 // Environment variables validation
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase environment variables");
