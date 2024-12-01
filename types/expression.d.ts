b84cc39388c7cf36730a8b17409bae3b3368f0e0
Xiangyu Kong <xkongtech@outlook.com>
2024-11-30 19:35:22 -0500

    add types definition

---
+++ types/expression.d.ts
@@ -0,0 +1,6 @@
+
+export interface Expression {
+  expression_id: string;
+  text: string;
+  translation: string;
+}
---
+++ types/video.d.ts
@@ -0,0 +1,13 @@
+export interface Video {
+  video_id: string;
+  title: string;
+  thumbnail_url: string;
+  view_count: string;
+  published_time: string;
+  duration: string;
+  description: string;
+  accessibility_label: string;
+  url: string;
+  topic_tag: string;
+  level_tag: string;
+}
