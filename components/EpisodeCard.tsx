b29da6650e0a46d8b8662de98355181af44fd0a1
XIANGYU KONG <xkongtech@outlook.com>
2024-12-26 17:16:30 +0800

    use Image component

--- components/EpisodeCard.tsx
+++ components/EpisodeCard.tsx
@@ -1,5 +1,6 @@
 import { Episode } from "@/app/types/Episode";
 import Link from "next/link";
+import Image from "next/image";
 
 interface EpisodeCardProps {
   episode: Episode;
@@ -24,9 +25,11 @@ export const EpisodeCard = ({ episode }: EpisodeCardProps) => {
       href={`/episodev2/${episode.episode_id}`}
       className="relative group rounded-lg overflow-hidden cursor-pointer"
     >
-      <img
+      <Image
         src={`/images/episodes/ep${episode.episode_id}.png`}
         alt={`Episode ${episode.episode_id}`}
+        width={800}
+        height={600}
         className="w-full h-48 object-cover"
       />
       <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/80 to-transparent">
